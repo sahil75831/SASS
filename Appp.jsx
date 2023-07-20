05780faf09d6a5e8cce26b3d77d6b67a8f11e67b
sahilsharma <sahilsharma@gmail.com>
2023-07-20 23:14:30 +0530

    first commit

---
+++ Appp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import First from './First'
+
+const Appp = () => {
+  return (
+    <div>
+      <First />
+    </div>
+  )
+}
+
+export default Appp
---
+++ First.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./app.scss"
+const First = () => {
+  return (
+    <div>
+      first
+      <div style={{backgroundColor:"rebeccapurple"}}>
+        second
+      </div>
+      <button className='login'> login</button>
+      <br></br>
+      <button className='login2'> logout</button>
+      <div className='div'>hello</div>
+    </div>
+  )
+}
+
+export default First
